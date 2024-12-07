8a8e20b74861c8fef13c06f696c5b75ba55c4a20
theicecub <banan8936@gmail.com>
2024-12-07 20:41:13 +0500

    commiting homework

--- hometask.py
+++ hometask.py
@@ -2,6 +2,6 @@ import inspect
 import colorama as c
 print(dir(c))
 print(type(c))
-print(hasattr(c, "Back"))
-print(getattr(c, "Style"))
-print(inspect.getdoc(c))
+print(hasattr(c, "Cursor")) # Показывает курсор
+print(getattr(c, "Style")) # Меняет стиль
+print(inspect.getdoc(c)) # Нет документации для этого модуля, поэтому в консоль выводится "None"
